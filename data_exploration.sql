f5cd1873adadcc90ebba8c7e127c1ac1abc40d3e
Sonukumarsingh <sonusingham0000@gmail.com>
2023-09-13 11:56:50 +0530

    update file

--- data_exploration.sql
+++ data_exploration.sql
@@ -1,1 +1,13 @@
-select * FROM orders;
+ /*INSTAGRAM CLONE EXPLORATORY DATA ANALYSIS USING SQL*/
+
+ /*SQL SKILLS: joins, date manipulation, regular expressions, views, stored procedures, aggregate functions, string manipulation*/
+ 
+-- --------------------------------------------------------------------------------------------------------------
+
+  /*Ques.1 The first 10 users on the platform*/
+
+SELECT 
+    *
+FROM
+    ig_clone.users
+ORDER BY created_at asc;
