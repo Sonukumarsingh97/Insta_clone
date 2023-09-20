d04b69208399ec10073c3522478feb5b86cb8085
Sonukumarsingh <sonusingham0000@gmail.com>
2023-09-20 14:33:13 +0530

    Ques.4 added

--- data_exploration.sql
+++ data_exploration.sql
@@ -14,18 +14,27 @@ ORDER BY created_at asc
 limit 10;
 
 
-  /*Ques.2 Total number of users*/
+/*Ques.2 Total number of users*/
 
-  SELECT count(*) as users
-   from ig_clone.users ;
+SELECT count(*) as users
+from ig_clone.users ;
    
 
-   /* Ques.3 most popular day of week for ragistration */
+/* Ques.3 most popular day of week for ragistration */
 
-   select DAYNAME(created_at) as day,
-    COUNT(id) as total
-    from ig_clone.users
-   GROUP BY day
-   ORDER BY total desc;
+select DAYNAME(created_at) as day,
+COUNT(id) as total
+from ig_clone.users
+GROUP BY day
+ORDER BY total desc;
+
+  
+/*Ques.4 The users who have never posted a photo*/
+
+SELECT username
+FROM ig_clone.users
+LEFT JOIN ig_clone.photos
+    ON users.id = photos.user_id
+WHERE photos.id IS NULL;
 
 
